a201fe409a885232d32417815e7db2cb489f737e
GitDisciplined <68583436+GitDisciplined@users.noreply.github.com>
2025-09-04 12:49:36 +0530

    Add files via upload

--- sol_ode.py
+++ sol_ode.py
@@ -17,7 +17,7 @@ def bergman(t,y):
     
     if (gamma*t*(y[0]-h)<0):
         dGdt = (-p1+y[2])*y[0]+p1*G_basal+np.random.normal(1,5)
-        dIdt = -n*y[1]+np.random.normal(2,5)
+        dIdt = -n*y[1]+np.random.normal(1,5)
         dXdt= -p2*y[2]+p3*(y[1]-I_basal)+np.random.normal(1,5)
     else:
         dGdt = (-p1+y[2])*y[0]+p1*G_basal+np.random.normal(1,5)
@@ -27,32 +27,16 @@ def bergman(t,y):
     return [dGdt,dIdt,dXdt]
 
 
-
 y0 = [1,1,1]
 
-t_span = (0, 10) # Solve from t=0 to t=10
-t_eval = np.linspace(t_span[0], t_span[1], 100) # Evaluate solution at 100 points
 
 
+t_span = (0, 1) 
 
-    
-solution = solve_ivp(bergman, t_span, y0,t_eval=t_eval)
+solution = solve_ivp(bergman, t_span, y0)
 
+y_value = solution.y[:,-1]
 
-time = solution.t
-y_values = solution.y
-
-#print(y_values)
-
-# Plot the results
-plt.plot(time, y_values[0], label='G(t) versus time')
-plt.plot(time, y_values[1], label='I(t) versus time')
-plt.plot(time, y_values[2], label='X(t) versus time')
+print(y_value)
 
 
-plt.xlabel('Time')
-plt.ylabel('Glucose/Insulin Concentration')
-plt.title('Solution of ODE System')
-plt.legend()
-plt.grid(True)
-plt.show()
